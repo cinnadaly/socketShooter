10a9c0dc8feb9df801132c5ba9ffbebf0737d7e0
Alejandro Vaquera Lopez <alejandrovaqueralopez@gmail.com>
2026-06-10 17:13:07 -0700

    feat: adding game mechanics

--- public/service/game.js
+++ public/service/game.js
@@ -18,9 +18,6 @@ let cursores;
 let espacio;
 let enter;
 
-let balas = [];
-let enemigos = [];
-
 let score = 0;
 let scoreText;
 
@@ -38,9 +35,6 @@ function create() {
         0x00ff00
     );
 
-    for(let i = 0; i < 5; i++) {
-        crearEnemigo.call(this);
-    }
 
     cursores = this.input.keyboard.createCursorKeys();
 
@@ -93,132 +87,21 @@ function update() {
         jugador.x += 6;
     }
 
-    if(
-        Phaser.Input.Keyboard.JustDown(
-            enter
-        )
-    ) {
-
-        const bala =
-            this.add.rectangle(
-                jugador.x,
-                jugador.y - 30,
-                5,
-                15,
-                0xffff00
-            );
-
-        balas.push(bala);
-    }
-
-    for(let i = balas.length - 1; i >= 0; i--) {
-
-        balas[i].y -= 15;
-
-        if(balas[i].y < 0) {
-
-            balas[i].destroy();
-
-            balas.splice(i, 1);
-        }
-    }
-
-    for(let i = enemigos.length - 1; i >= 0; i--) {
-
-        enemigos[i].y += 4;
-
-        if(enemigos[i].y > 750) {
-
-            enemigos[i].destroy();
-
-            enemigos.splice(i, 1);
-
-            crearEnemigo.call(this);
-        }
-    }
-
-    for(let i = enemigos.length - 1; i >= 0; i--) {
-
-        for(let j = balas.length - 1; j >= 0; j--) {
-
-            if(colision(
-                enemigos[i],
-                balas[j]
-            )) {
-
-                enemigos[i].destroy();
-                balas[j].destroy();
-
-                enemigos.splice(i, 1);
-                balas.splice(j, 1);
-
-                crearEnemigo.call(this);
-
-                score++;
-
-                scoreText.setText(
-                    "Score: " + score
-                );
-
-                break;
-            }
-        }
-    }
-
-    for(const enemigo of enemigos) {
-
-        if(
-            colision(
-                jugador,
-                enemigo
-            )
-        ) {
-
-            mostrarGameOver.call(this);
-        }
+    if(Phaser.Input.Keyboard.JustDown(enter)) {
+        console.log("dispara")
     }
 }
 
-function crearEnemigo() {
-
-    const enemigo =
-        this.add.rectangle(
-            Phaser.Math.Between(
-                25,
-                475
-            ),
-            Phaser.Math.Between(
-                -500,
-                -50
-            ),
-            50,
-            50,
-            0xff0000
-        );
-
-    enemigos.push(
-        enemigo
-    );
-}
 
 function colision(a, b) {
 
+    const aBox = a.getBounds();
+    const bBox = b.getBounds();
     return (
-        a.getBounds().x <
-            b.getBounds().x +
-            b.getBounds().width &&
-
-        a.getBounds().x +
-            a.getBounds().width >
-            b.getBounds().x &&
-
-        a.getBounds().y <
-            b.getBounds().y +
-            b.getBounds().height &&
-
-        a.getBounds().y +
-            a.getBounds().height >
-            b.getBounds().y
+        aBox.x < bBox.x + bBox.width &&
+        aBox.x + aBox.width > bBox.x &&
+        aBox.y < bBox.y + bBox.height &&
+        aBox.y + aBox.height > bBox.y
     );
 }
 
@@ -263,23 +146,8 @@ function reiniciar() {
         "Score: 0"
     );
 
-    for(const bala of balas) {
-        bala.destroy();
-    }
-
-    for(const enemigo of enemigos) {
-        enemigo.destroy();
-    }
-
-    balas = [];
-    enemigos = [];
-
     jugador.x = 250;
     jugador.y = 620;
-
-    for(let i = 0; i < 5; i++) {
-        crearEnemigo.call(this);
-    }
 
     gameOverText.destroy();
     restartText.destroy();
